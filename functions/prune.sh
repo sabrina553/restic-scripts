a78762b400b5763b624ac699df6d3bcc1914ec8d
sabrina553 <sabrinalily@pm.me>
2025-10-26 20:46:16 +0000

    I guess for now I will just use two seperate repos for immich and main on sapphire. the whole tag thing is messy

--- functions/prune.sh
+++ functions/prune.sh
@@ -7,25 +7,12 @@ declare -n REPO=${1}_REPOSITORY_FILE
 declare -n PASS=${1}_PASSWORD_FILE
 declare -n POLICY=${1}_REMOVE_POLICY
 
-#IFS=',' read -a tags <<< "$2"
-
 echo ""
 echo "Forgetting and pruning ${1} snapshots according to retention policy ($(date))..."
 echo "Policy: ${POLICY}"
 echo "---------------------------"
 
-
-restic --repository-file="$REPO" --password-file="$PASS" forget --keep-tag "$2" "$POLICY" --prune
-
-
-#### not sure yet how I should be handling multiple tags in one repo 
-# for tag in "${tags[@]}"
-# do 
-#     restic --repository-file=${REPO} --password-file=${PASS} forget --keep-tag ${tag} ${POLICY} --prune
-# done
-
-
-
+restic --repository-file="$REPO" --password-file="$PASS" forget ${POLICY} --prune
 
 echo ""
 echo "Finished forget and prune $(date)"
