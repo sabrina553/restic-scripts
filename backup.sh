803a38ec2e2b85f4858b4b99cbd10e91fcf46c88
sabrina553 <sabrinalily@pm.me>
2025-10-26 16:43:46 +0000

    Dry-run for dev environment, also just trying to have branches work pls

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ echo "Running scheduled backup ($(date))..."
 echo "---------------------------"
 
 
-restic --repository-file=$LOCAL_REPOSITORY_FILE --password-file=$LOCAL_PASSWORD_FILE backup --tag main --exclude-file=$RESTIC_EXCLUDE_FILE_PATH --files-from=$RESTIC_INCLUDE_FILE_PATH 
+restic --repository-file=$LOCAL_REPOSITORY_FILE --password-file=$LOCAL_PASSWORD_FILE backup --tag main --exclude-file=$RESTIC_EXCLUDE_FILE_PATH --files-from=$RESTIC_INCLUDE_FILE_PATH --dry-run
 
 echo ""
 echo "Finished scheduled backup $(date)"
--- backup_immich.sh
+++ backup_immich.sh
@@ -7,7 +7,7 @@ echo "Running scheduled backup of immich ($(date))..."
 echo "---------------------------"
 
 
-restic --repository-file=$SAPPHIRE_REPOSITORY_FILE --password-file=$SAPPHIRE_PASSWORD_FILE backup --tag immich /media/sabrina/immich 
+restic --repository-file=$SAPPHIRE_REPOSITORY_FILE --password-file=$SAPPHIRE_PASSWORD_FILE backup --tag immich /media/sabrina/immich --dry-run
 
 echo ""
 echo "Finished scheduled backup $(date)"
