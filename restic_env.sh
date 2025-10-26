e4b715fd13dd6273811d1926acba44eb8ffac775
sabrina553 <sabrinalily@pm.me>
2025-10-26 17:30:32 +0000

    add prune policy to env, as this might be a variable policy for each node

--- restic_env.sh
+++ restic_env.sh
@@ -8,11 +8,14 @@ export RESTIC_CACHE_DIR="/root/.cache/restic/"
 # local info
 export LOCAL_REPOSITORY_FILE="/srv/restic-repo/scripts/Identities/LOCAL_REPO"
 export LOCAL_PASSWORD_FILE="/srv/restic-repo/scripts/Identities/LOCAL_PASSWORD"
+export LOCAL_REMOVE_POLICY"--keep-daily 30 --keep-weekly 8 --keep-monthly 12 --keep-yearly 100 --keep-last 4 --prune"
 
 # raspberry pi info
 export RPI_REPOSITORY_FILE="/srv/restic-repo/scripts/Identities/RPI_REPO"
 export RPI_PASSWORD_FILE="/srv/restic-repo/scripts/Identities/RPI_PASSWORD"
+export RPI_REMOVE_POLICY"--keep-daily 30 --keep-weekly 8 --keep-monthly 12 --keep-yearly 100 --keep-last 4 --prune"
 
 # sapphire pi info
 export SAPPHIRE_REPOSITORY_FILE="/srv/restic-repo/scripts/Identities/SAPPHIRE_REPO"
 export SAPPHIRE_PASSWORD_FILE="/srv/restic-repo/scripts/Identities/SAPPHIRE_PASSWORD"
+export SAPPHIRE_REMOVE_POLICY"--keep-daily 30 --keep-weekly 8 --keep-monthly 12 --keep-yearly 100 --keep-last 4 --prune"
