6bb572d4f7f06d4ebcf1b6b346af353075ac3ced
sabrina553 <sabrinalily@pm.me>
2025-10-26 19:06:14 +0000

    clean up

--- snapshots.sh
+++ snapshots.sh
@@ -6,4 +6,4 @@
 declare -n REPO=${1}_REPOSITORY_FILE
 declare -n PASS=${1}_PASSWORD_FILE
 
-restic --repository-file=${REPO} --password-file=${PASS} snapshots
+restic --repository-file="$REPO" --password-file="$PASS" snapshots
--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./env.sh
+. /srv/restic-repo/scripts/env.sh
 
 declare -n REPO=${1}_REPOSITORY_FILE
 declare -n PASS=${1}_PASSWORD_FILE
@@ -10,7 +10,7 @@ echo "-"
 echo "Attempting to unlock ${1} repo"
 echo "---------------------------"
 
-restic --repository-file=${REPO} --password-file=${PASS} unlock --cleanup-cache
+restic --repository-file="$REPO" --password-file="$PASS" unlock --cleanup-cache
 
 echo ""
 echo "Finished unlocking $(date)"
