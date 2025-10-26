a8365e1954053755194b1805d2b75adebcbf3c3a
sabrina553 <sabrinalily@pm.me>
2025-10-26 17:01:21 +0000

    Made universal for all nodes i;e ./check.sh LOCAL

--- check.sh
+++ check.sh
@@ -4,14 +4,18 @@
 export GOGC=20
 
 # run unlock script first (includes setting env vars)
-. /srv/restic-repo/scripts/local_unlock.sh
+. /srv/restic-repo/scripts/unlock.sh ${1}
+
+declare -n REPO=${1}_REPOSITORY_FILE
+declare -n PASS=${1}_PASSWORD_FILE
+
 
 echo "-"
 echo "-"
 echo "Checking backup ($(date))..."
 echo "---------------------------"
 
-restic --repository-file=$LOCAL_REPOSITORY_FILE --password-file=$LOCAL_PASSWORD_FILE check --cache-dir "${RESTIC_CACHE_DIR}" --cleanup-cache
+restic --repository-file=$REPO --password-file=$PASS check --cache-dir "${RESTIC_CACHE_DIR}" --cleanup-cache
 
 echo ""
 echo "Finished checking backup $(date)"
