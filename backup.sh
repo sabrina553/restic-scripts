0697db5d7bb35e588b83870bd6176ba39ae06ac8
sabrina553 <sabrinalily@pm.me>
2025-10-26 18:45:08 +0000

    read for two input variables, to be universal for any backup stratergy

--- backup.sh
+++ backup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 . /srv/restic-repo/scripts/unlock.sh "$1"
 
+#read input variable for repo
 declare -n REPO=${1}_REPOSITORY_FILE
 declare -n PASS=${1}_PASSWORD_FILE
+#read input variable for snapshot
+declare -n EXCLUDE=${2}_EXCLUDE_FILE_PATH
+declare -n INCLUDE=${2}_INCLUDE_FILE_PATH
 
-echo "-"
+#"Aesthetics"
 echo "-"
 echo "Running scheduled backup ($(date))..."
 echo "---------------------------"
 
 
-restic --repository-file=$REPO --password-file=$PASS backup --tag main --exclude-file=$RESTIC_EXCLUDE_FILE_PATH --files-from=$RESTIC_INCLUDE_FILE_PATH
+restic --repository-file="$REPO" --password-file="$PASS" backup --tag main --exclude-file="$EXCLUDE" --files-from="$INCLUDE" --dry-run
 
 echo ""
 echo "Finished scheduled backup $(date)"
